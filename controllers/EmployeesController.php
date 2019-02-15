d2aac90566e7da248407fdf9d983bb18489ee6f0
Daniel Lopes <viana.daniel02@gmail.com>
2019-02-15 17:03:01 -0300

    Adicionando Serializer

--- controllers/EmployeesController.php
+++ controllers/EmployeesController.php
@@ -8,4 +8,9 @@ class EmployeesController extends ActiveController
 {
     public $modelClass = "\app\models\Employee";
 
+    public $serializer = [
+        'class' => 'yii\rest\Serializer',
+        'collectionEnvelope' => 'data'
+    ];
+
 }
